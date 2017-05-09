b3c649b55c9f1782f40ef121286844354e86eb33
JohnNolan5 <jnolan5@nd.edu>
2017-05-09 19:46:10 -0400

    Win and loss diplays

--- character.py
+++ character.py
@@ -158,7 +158,9 @@ class GameSpace:
         Thread(target=reactor.run, args=(False, )).start()
         clear_old_char = False
         stations_won = 0
-        self.won = False
+        global lost
+        lost_display = lost
+        win_display = False
         while 1:
             #global stations_sent
             #if c_made and not stations_sent:
@@ -234,7 +236,7 @@ class GameSpace:
                     stations_won += 1
                     if stations_won == len(stations):
                         self.sendWon()
-                        self.won = True
+                        win_display = True
                         print("YOU WON!") 
                 if counter > num_stations-1:
                     break
@@ -263,6 +265,23 @@ class GameSpace:
             old_pos = pos
             old_char = rect
             old_light = self.light.light.get_rect(center=(self.light.light_rect.x+light_size/2+1, self.light.light_rect.y+light_size/2+1))
+            if lost_display:
+                pygame.font.init()
+                myfont = pygame.font.SysFont('Comic Sans MS', 50)
+                textsurface = myfont.render("Sorry. You've lost!", True, (0,250,250))
+                self.screen.blit(textsurface, (250,250))
+                pygame.display.flip()
+            elif win_display:
+                pygame.font.init()
+                myfont = pygame.font.SysFont('Comic Sans MS', 30)
+                textsurface = myfont.render("Congratulations. You've won!", True, (0,250,250))
+                myfont2 = pygame.font.SysFont('Comic Sans MS', 50)
+                textsurface2 = myfont.render("Press q to quit. Press p to play again.", True, (0,250,250))
+                self.screen.blit(textsurface, (250,250))
+                self.screen.blit(textsurface2, (250,350))
+                pygame.display.flip()
+            
+                
 
 if __name__=='__main__':
     reactor.listenTCP(10130, MyCommandConnectionFactory())
--- light.py
+++ light.py
@@ -21,6 +21,7 @@ enemy_y = 150
 stations = []
 c_made = False
 rec_stations = False
+lost_display = False
 ########## COMMAND CONNECTION ##################
 class MyCommandConnection(Protocol):
     def connectionMade(self):
@@ -50,6 +51,7 @@ class MyCommandConnection(Protocol):
                 st_rect = Rect(int(data[1]), int(data[2]), 70, 70)
                 stations.append(st_rect)
             elif data[0] == b"W":
+                global lost_display = True
                 print("YOU LOST")
 
 class MyCommandConnectionFactory(ClientFactory):
@@ -220,7 +222,14 @@ class GameSpace:
             old_rects = active_rects[:] 
             old_pos = pos
             old_light = self.light.light.get_rect(center=(self.light.light_rect.x+light_size/2+1, self.light.light_rect.y+light_size/2+1))
-            if win_display:
+            global lost_display
+            if lost_display:
+                pygame.font.init()
+                myfont2 = pygame.font.SysFont('Comic Sans MS', 50)
+                textsurface = myfont.render("Sorry. You've Lost!", True, (0,250,250))
+                self.screen.blit(textsurface, (250,250))
+                pygame.display.flip() 
+            elif win_display:
                 pygame.font.init()
                 myfont = pygame.font.SysFont('Comic Sans MS', 30)
                 textsurface = myfont.render("Congratulations. You've won!", True, (0,250,250))
